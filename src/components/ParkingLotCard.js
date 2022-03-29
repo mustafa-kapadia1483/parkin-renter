c390d327f37d6a57751b634177f2aa3a6b70106b
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-03-29 10:40:13 +0530

    alert dialog and card buttons

--- src/components/ParkingLotCard.js
+++ src/components/ParkingLotCard.js
@@ -1,8 +1,60 @@
-import { Box, HStack, Text, useColorModeValue, VStack } from '@chakra-ui/react';
+import {
+  Button,
+  Box,
+  ButtonGroup,
+  HStack,
+  Text,
+  useColorModeValue,
+  VStack,
+  useDisclosure,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+} from '@chakra-ui/react';
+import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
+import { useRef } from 'react';
 
 const ParkingLotCard = ({ parkingLot }) => {
   const { name, latitude, longitude, address, price } = parkingLot;
   const { streetAddress, locality, landmark, city, pincode } = address;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef();
+  const CardAlertDialog = () => {
+    return (
+      <>
+        <AlertDialog
+          isOpen={isOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={onClose}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Delete Customer
+              </AlertDialogHeader>
+
+              <AlertDialogBody>
+                Are you sure? You can't undo this action afterwards.
+              </AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button colorScheme="red" onClick={onClose} ml={3}>
+                  Delete
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </>
+    );
+  };
+
   return (
     <Box
       px={8}
@@ -23,6 +75,19 @@ const ParkingLotCard = ({ parkingLot }) => {
           {pincode}
         </Text>
         <Text>Price: Rs. {price}/hr</Text>
+        <ButtonGroup variant="outline" spacing="6">
+          <Button colorScheme="blue" leftIcon={<AiOutlineEdit />}>
+            Edit
+          </Button>
+          <Button
+            onClick={onOpen}
+            colorScheme="red"
+            leftIcon={<AiOutlineDelete />}
+          >
+            Delete
+          </Button>
+        </ButtonGroup>
+        <CardAlertDialog />
       </VStack>
     </Box>
   );
